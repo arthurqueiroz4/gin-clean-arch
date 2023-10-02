0bf2656127d8b6ffd5d08ec912f8e0dbab27aafd
Arthur <arthur.queiroz08@aluno.ifce.edu.br>
2023-10-01 22:16:37 -0300

    formantando erros

--- internal/tokenutil.go
+++ internal/tokenutil.go
@@ -28,8 +28,8 @@ func CreateAccessToken(employee *models.Employee, secret string, expiry int) (ac
 
 func CreateRefreshToken(employee *models.Employee, secret string, expiry int) (refreshToken string, err error) {
 	claimsRefresh := &domain.JwtCustomRefreshClaims{
-		strconv.Itoa(int(employee.Model.ID)),
-		jwt.StandardClaims{
+		ID: strconv.Itoa(int(employee.Model.ID)),
+		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(time.Hour * time.Duration(expiry)).Unix(),
 		},
 	}
@@ -45,7 +45,7 @@ func IsAuthorized(requestToken string, secret string) (bool, error) {
 	_, err := jwt.Parse(requestToken,
 		func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 			}
 			return []byte(secret), nil
 		})
@@ -59,7 +59,7 @@ func ExtractIDFromToken(requestToken string, secret string) (string, error) {
 	token, err := jwt.Parse(requestToken,
 		func(token *jwt.Token) (interface{}, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected siging method: %v", token.Header["alg"])
+				return nil, fmt.Errorf("unexpected siging method: %v", token.Header["alg"])
 			}
 			return []byte(secret), nil
 		})
@@ -69,7 +69,7 @@ func ExtractIDFromToken(requestToken string, secret string) (string, error) {
 	claims, ok := token.Claims.(jwt.MapClaims)
 
 	if !ok && !token.Valid {
-		return "", fmt.Errorf("Invalid token")
+		return "", fmt.Errorf("invalid token")
 	}
 	return claims["id"].(string), nil
 }
